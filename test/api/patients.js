7185155a90ba3bacac9bcb84f47d587309ce41c9
Alex Beloshevsky <alexbeloshevsky@gmail.com>
2020-02-14 17:09:13 +0200

    added another test to the GET route

--- test/api/patients.js
+++ test/api/patients.js
@@ -60,4 +60,25 @@ describe("GET /patients/all", function() {
         expect(docs.body.length).to.be.equal(0);
       });
   });
+
+  it("returns a list of Patients with 1 patient", async function() {
+    const ownerName = "Alex Beloshevsky";
+    await request(app)
+      .post("/patients/create")
+      .send({
+        petName: "Blackie",
+        petType: "Dog",
+        ownerName,
+        ownerPhoneNumber: "0506678419"
+      });
+
+    const docs = await request(app)
+      .get("/patients/all")
+      .expect(200)
+      .expect("Content-Type", /json/);
+
+    expect(Array.isArray(docs.body)).to.be.equal(true);
+    expect(docs.body.length).to.be.equal(1);
+    expect(docs.body[0].ownerName).to.be.equal(ownerName);
+  });
 });
